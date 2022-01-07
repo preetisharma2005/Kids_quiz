7690974f9ccce037c1208b2d3ecd2e93f97f59eb
preetisharma2005 <93264428+preetisharma2005@users.noreply.github.com>
2022-01-07 15:11:28 +0530

    Code to calculate  grades.

---
+++ yourgrade.py
@@ -0,0 +1,14 @@
+# Check your grade.
+
+g = eval(input("Enter your number: "))
+
+if g >= 90:
+    print(" You got grade A! :)")
+elif g >= 80 :
+    print(" You got grade B.")
+elif g>=70 :
+    print("You got grade C.")
+elif g>=60 :
+    print("You got grade D.")
+else:
+    print("You got F :( ")
